85e6ad12a7a50ad02caf6753aac7f71685bd497c
Sakib8git <sakib30278@gnail.com>
2025-11-12 00:05:45 +0600

    add .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,8 +12,7 @@ app.get("/", (req, res) => {
   res.send("Habit tracker is Running");
 });
 
-const uri =
-  "mongodb+srv://habit-tracker-db:zCNlsBQlJIifbJwj@cluster0.kyh1mx2.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kyh1mx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
